1cba90308746bab12da6139ee6883def656bfbc3
aginies <aginies@suse.com>
2024-05-15 16:51:51 +0200

    various improvment an cleanup

--- easy_tag.sh
+++ easy_tag.sh
@@ -1,35 +1,57 @@
 #!/bin/bash
+# antoine@ginies.org
+
 WDATE=`date +20%y%m%d`
-TEXTTOINCLUDE="MAI_2024_ERASMUS"
+TEXTTOINCLUDE="TAG_TO_INCLUDE"
+TEXTTOINCLUDE=${TEXTTOINCLUDE// /_}
 ORIG=$1
 
+# Check ImageMagick is installed
+if [ ! -f "/usr/bin/convert"  ];then
+	echo "Please install ImageMagick"
+	exit 1
+fi
+
+# Need first parameter (image to tag)
 if [ -z "$1" ];then
 	echo "Need the the file to modify!"
 	exit 1
 fi
 
+# Generate a random number between 0 and 11 (inclusive)
+RANDOM_011=$((RANDOM % 12))
+# Add 19 to the random number to get a range between 19 and 30
+RANDOM_030=$((RANDOM_011 + 19))
+
 convert -background None -fill "rgba(64, 64, 64, 0.50)" \
     -pointsize 32 label:"$TEXTTOINCLUDE" \
-    -rotate -20 \
+    -rotate -${RANDOM_030} \
     +repage +write mpr:TILE \
     +delete $ORIG -alpha set \( +clone -fill mpr:TILE -draw "color 0,0 reset" \) \
     -composite readytmp_$ORIG 
 
-sync
+# not a good idea on some system...
+#sync
+
+RANDOM_070=$((RANDOM_011 + 70))
 
 convert -background None -fill "rgba(128, 34, 34, 0.35)" \
     -pointsize 16 label:"$TEXTTOINCLUDE" \
-    -rotate -70 \
+    -rotate -${RANDOM_070} \
     +repage +write mpr:TILE \
     +delete readytmp_$ORIG -alpha set \( +clone -fill mpr:TILE -draw "color 0,0 reset" \) \
     -composite readytmp2_${ORIG}
 
+# Reduce quality to 70%
 convert -quality 70% readytmp2_${ORIG} ready_${ORIG}
 
 filename=$(basename "$ready_${ORIG}")
 extension="${filename##*.}"
 basen="${filename%.*}"
-newname="${basen}_${TEXTTOINCLUDE}.${extension}"
+newname="${basen}_${TEXTTOINCLUDE}_${DATE}.${extension}"
+# rename to a correct name
 mv ready_${ORIG} ${newname}
+# remove tmp file
+rm -f readytmp*_${ORIG}
+# list the file to use :)
 ls -1 ${newname}
-rm -vf readytmp*_${ORIG}
